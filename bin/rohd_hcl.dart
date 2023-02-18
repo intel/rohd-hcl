9bfbe0cdf8e966bf79167cbba8786a07013a52a2
Max Korbel <max.korbel@intel.com>
2023-02-17 16:17:21 -0800

    First commit, initial dart project

---
+++ bin/rohd_hcl.dart
@@ -0,0 +1,5 @@
+import 'package:rohd_hcl/rohd_hcl.dart' as rohd_hcl;
+
+void main(List<String> arguments) {
+  print('Hello world: ${rohd_hcl.calculate()}!');
+}
---
+++ test/rohd_hcl_test.dart
@@ -0,0 +1,8 @@
+import 'package:rohd_hcl/rohd_hcl.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
