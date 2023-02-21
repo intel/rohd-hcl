dcaa100952a703df7cdc320e359f97341b086a8c
Max Korbel <max.korbel@intel.com>
2023-02-21 13:46:42 -0800

    got rotate left working

--- lib/src/rotate_left.dart
+++ lib/src/rotate_left.dart
@@ -6,27 +6,67 @@
 // 2023 February 17
 //
 
+import 'dart:math';
+
 import 'package:rohd/rohd.dart';
 
+class RotateLeft extends Module {
+  final int maxAmount;
+
+  Logic get rotated => output('rotated');
+
+  Logic get _original => input('original');
+
+  /// TODO
+  RotateLeft(Logic original, Logic rotateAmount, {int? maxAmount})
+      : maxAmount = min(
+          maxAmount ?? original.width,
+          pow(2, rotateAmount.width).toInt() - 1,
+        ) {
+    original = addInput('original', original, width: original.width);
+    rotateAmount =
+        addInput('rotate_amount', rotateAmount, width: rotateAmount.width);
+
+    addOutput('rotated', width: original.width);
+
+    Combinational([
+      Case(rotateAmount, conditionalType: ConditionalType.unique, [
+        for (var i = 1; i < this.maxAmount; i++)
+          CaseItem(
+            Const(i, width: rotateAmount.width),
+            [rotated < _rotateBy(i, original)],
+          )
+      ], defaultItem: [
+        rotated < original,
+      ])
+    ]);
+  }
+
+  static Logic _rotateBy(int amount, Logic original) {
+    final split = original.width - amount % original.width;
+    if (split == original.width) {
+      return original;
+    }
+
+    return [
+      original.getRange(0, split),
+      original.getRange(split),
+    ].swizzle();
+  }
+}
+
 extension RotateLeftLogic on Logic {
-  Logic rotateLeft(dynamic amount) {
-    // TODO: move this to a module?
+  ///TODO
+  Logic rotateLeft(dynamic amount, {int? maxAmount}) {
     if (amount is int) {
-      final amountModulo = amount % width;
+      assert(
+          maxAmount == null || amount <= maxAmount,
+          'If `maxAmount` is provided with an integer `amount`,'
+          ' it should meet the restriction.');
 
-      if (amountModulo == 0) {
-        return this;
-      }
-
-      return [
-        getRange(0, amountModulo),
-        getRange(amountModulo),
-      ].swizzle();
+      return RotateLeft._rotateBy(amount, this);
     } else if (amount is Logic) {
-      final amountModulo = amount % width;
-
-      return (this << amountModulo) |
-          (this >> (Const(width, width: width) - amountModulo));
+      return RotateLeft(this, amount, maxAmount: maxAmount).rotated;
     } else {
       // TODO: make an HCL type of exception for this
       throw Exception('Unknown type for amount: ${amount.runtimeType}');
--- test/rotate_left_test.dart
+++ test/rotate_left_test.dart
@@ -12,9 +12,18 @@ import 'package:test/test.dart';
 
 void main() {
   group('Logic', () {
-    test('Rotate left by int', () {
+    test('Rotate left by int', () async {
       final orig = Logic(width: 8)..put(0xf0);
-      expect(orig.rotateLeft(4).value.toInt(), equals(0xf0));
+
+      print(orig.rotateLeft(0).value.toString(includeWidth: false));
+      print(orig.rotateLeft(1).value.toString(includeWidth: false));
+      print(orig.rotateLeft(2).value.toString(includeWidth: false));
+
+      var mod = RotateLeft(orig, Logic(width: 8));
+      await mod.build();
+      print(mod.generateSynth());
+
+      expect(orig.rotateLeft(4).value.toInt(), equals(0x0f));
       expect(orig.rotateLeft(1).value.toInt(), equals(0xe1));
     });
     test('Rotate left by Logic', () {});
