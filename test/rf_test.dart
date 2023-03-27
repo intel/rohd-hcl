eeaa1f52788eba1e2e869f4c433f01b1c4418d4c
Max Korbel <max.korbel@intel.com>
2023-03-27 14:22:52 -0700

    Fix strobe bug and add test (#6)

--- test/rf_test.dart
+++ test/rf_test.dart
@@ -73,6 +73,63 @@ void main() {
     await Simulator.simulationEnded;
   });
 
+  test('rf wr strobe', () async {
+    const dataWidth = 32;
+    const addrWidth = 5;
+
+    const numWr = 1;
+    const numRd = 1;
+
+    final clk = SimpleClockGenerator(10).clk;
+    final reset = Logic();
+
+    final wrPorts = [
+      for (var i = 0; i < numWr; i++)
+        StrobeDataPortInterface(dataWidth, addrWidth)..en.put(0)
+    ];
+    final rdPorts = [
+      for (var i = 0; i < numRd; i++)
+        DataPortInterface(dataWidth, addrWidth)..en.put(0)
+    ];
+
+    final rf = RegisterFile(clk, reset, wrPorts, rdPorts, numEntries: 20);
+
+    await rf.build();
+
+    unawaited(Simulator.run());
+
+    // a little reset flow
+    await clk.nextNegedge;
+    reset.put(1);
+    await clk.nextNegedge;
+    await clk.nextNegedge;
+    reset.put(0);
+    await clk.nextNegedge;
+    await clk.nextNegedge;
+
+    // write to addr 0x4 on port 0
+    wrPorts[0].en.put(1);
+    wrPorts[0].strobe.put(bin('1010'));
+    wrPorts[0].addr.put(4);
+    wrPorts[0].data.put(0xffffffff);
+
+    await clk.nextNegedge;
+    wrPorts[0].en.put(0);
+    await clk.nextNegedge;
+
+    // read it back out on a different port
+    rdPorts[0].en.put(1);
+    rdPorts[0].addr.put(4);
+    expect(rdPorts[0].data.value.toInt(), 0xff00ff00);
+
+    await clk.nextNegedge;
+    rdPorts[0].en.put(0);
+    await clk.nextNegedge;
+
+    Simulator.endSimulation();
+    await Simulator.simulationEnded;
+  });
+
   group('rf exceptions', () {
     test('mismatch addr width', () {
       expect(
