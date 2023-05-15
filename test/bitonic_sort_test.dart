99d90434aca5d0ae6c890f4676414a55efe83026
Yao Jing Quek <yao.jing.quek@intel.com>
2023-05-15 11:26:37 +0800

    Add random seed generator test

--- test/bitonic_sort_test.dart
+++ test/bitonic_sort_test.dart
@@ -11,6 +11,7 @@
 // ignore_for_file: cascade_invocations
 
 import 'dart:async';
+import 'dart:math';
 
 import 'package:rohd/rohd.dart';
 import 'package:rohd_hcl/rohd_hcl.dart';
@@ -21,95 +22,227 @@ Future<void> main() async {
     await Simulator.reset();
   });
 
+  List<int> generateNonDuplicateRand(int seed, int maxLength, int listLength) {
+    final rand = Random(seed);
+    return List<int>.generate(listLength, (index) {
+      int number;
+      final duplicateList = <int>[];
+      do {
+        number = rand.nextInt(maxLength);
+      } while (duplicateList.contains(number));
+      duplicateList.add(number);
+      return number;
+    });
+  }
+
   group('Bitonic Sort', () {
-    test(
-        'should return the sorted results in ascending order '
-        'given descending order', () async {
-      const dataWidth = 8;
+    group('Ascending Order: ', () {
+      test(
+          'should return the sorted results in ascending order '
+          'given descending order', () async {
+        const dataWidth = 8;
 
-      final clk = SimpleClockGenerator(10).clk;
-      final reset = Logic(name: 'reset');
+        final clk = SimpleClockGenerator(10).clk;
+        final reset = Logic(name: 'reset');
 
-      final x = <Logic>[
-        Const(16, width: dataWidth),
-        Const(15, width: dataWidth),
-        Const(14, width: dataWidth),
-        Const(13, width: dataWidth),
-        Const(12, width: dataWidth),
-        Const(11, width: dataWidth),
-        Const(10, width: dataWidth),
-        Const(9, width: dataWidth),
-        Const(8, width: dataWidth),
-        Const(7, width: dataWidth),
-        Const(6, width: dataWidth),
-        Const(5, width: dataWidth),
-        Const(4, width: dataWidth),
-        Const(3, width: dataWidth),
-        Const(2, width: dataWidth),
-        Const(1, width: dataWidth),
-      ];
+        final toSort =
+            List.generate(16, (index) => Const(16 - index, width: dataWidth));
 
-      final topMod = BitonicSort(clk, reset, toSort: x, name: 'top_level');
-      await topMod.build();
+        final topMod =
+            BitonicSort(clk, reset, toSort: toSort, name: 'top_level');
+        await topMod.build();
 
-      reset.inject(0);
+        reset.inject(0);
 
-      Simulator.registerAction(100, () {
-        for (var i = 0; i < topMod.sorted.length; i++) {
-          expect(topMod.sorted[i].value.toInt(), i + 1);
-        }
+        Simulator.registerAction(100, () {
+          for (var i = 0; i < topMod.sorted.length; i++) {
+            expect(topMod.sorted[i].value.toInt(), i + 1);
+          }
+        });
+
+        Simulator.setMaxSimTime(100);
+        WaveDumper(topMod, outputPath: 'test/recursive_list.vcd');
+
+        await Simulator.run();
       });
 
-      Simulator.setMaxSimTime(100);
-      WaveDumper(topMod, outputPath: 'test/recursive_list.vcd');
+      test(
+          'should return the sorted results in ascending order given '
+          'random seed number with duplicate', () async {
+        const dataWidth = 8;
+        final rand = Random(10);
 
-      await Simulator.run();
+        final clk = SimpleClockGenerator(10).clk;
+        final reset = Logic(name: 'reset');
+
+        final toSortInt = <int>[];
+        final toSort = List<Logic>.generate(pow(2, 3).toInt(), (index) {
+          final number = rand.nextInt(pow(2, 3).toInt() + 1);
+          toSortInt.add(number);
+          return Const(number, width: dataWidth);
+        });
+
+        final topMod =
+            BitonicSort(clk, reset, toSort: toSort, name: 'top_level');
+        await topMod.build();
+
+        reset.inject(0);
+
+        toSortInt.sort();
+        Simulator.registerAction(100, () {
+          for (var i = 0; i < topMod.sorted.length; i++) {
+            expect(topMod.sorted[i].value.toInt(), toSortInt[i]);
+          }
+        });
+
+        Simulator.setMaxSimTime(100);
+        WaveDumper(topMod, outputPath: 'test/recursive_list.vcd');
+
+        await Simulator.run();
+      });
+
+      test(
+          'should return the sorted results in ascending order given '
+          'random seed number without duplicate', () async {
+        const dataWidth = 8;
+        const seed = 10;
+
+        final listLength = pow(2, 3).toInt();
+        final maxRandNum = listLength;
+
+        final clk = SimpleClockGenerator(10).clk;
+        final reset = Logic(name: 'reset');
+
+        final toSortInt =
+            generateNonDuplicateRand(seed, maxRandNum, listLength);
+
+        final toSort = <Logic>[];
+        for (final num in toSortInt) {
+          toSort.add(Const(num, width: dataWidth));
+        }
+
+        final topMod =
+            BitonicSort(clk, reset, toSort: toSort, name: 'top_level');
+        await topMod.build();
+
+        reset.inject(0);
+
+        toSortInt.sort();
+        Simulator.registerAction(100, () {
+          for (var i = 0; i < topMod.sorted.length; i++) {
+            expect(topMod.sorted[i].value.toInt(), toSortInt[i]);
+          }
+        });
+
+        Simulator.setMaxSimTime(100);
+
+        await Simulator.run();
+      });
     });
 
-    test(
-        'should return the sorted results in descending order '
-        'given ascending order', () async {
-      const dataWidth = 8;
-      const direction = 0;
+    group('Descending Order: ', () {
+      test(
+          'should return the sorted results in descending order '
+          'given ascending order', () async {
+        const dataWidth = 8;
 
-      final clk = SimpleClockGenerator(10).clk;
-      final reset = Logic(name: 'reset');
+        final clk = SimpleClockGenerator(10).clk;
+        final reset = Logic(name: 'reset');
 
-      final x = <Logic>[
-        Const(1, width: dataWidth),
-        Const(2, width: dataWidth),
-        Const(3, width: dataWidth),
-        Const(4, width: dataWidth),
-        Const(5, width: dataWidth),
-        Const(6, width: dataWidth),
-        Const(7, width: dataWidth),
-        Const(8, width: dataWidth),
-        Const(9, width: dataWidth),
-        Const(10, width: dataWidth),
-        Const(11, width: dataWidth),
-        Const(12, width: dataWidth),
-        Const(13, width: dataWidth),
-        Const(14, width: dataWidth),
-        Const(15, width: dataWidth),
-        Const(16, width: dataWidth),
-      ];
+        final toSort =
+            List.generate(16, (index) => Const(index + 1, width: dataWidth));
 
-      final topMod = BitonicSort(clk, reset,
-          isAscending: false, toSort: x, name: 'top_level');
-      await topMod.build();
+        final topMod = BitonicSort(clk, reset,
+            isAscending: false, toSort: toSort, name: 'top_level');
+        await topMod.build();
 
-      reset.inject(0);
+        reset.inject(0);
 
-      Simulator.registerAction(100, () {
-        for (var i = 0; i < topMod.sorted.length; i++) {
-          expect(topMod.sorted[i].value.toInt(), topMod.sorted.length - i);
-        }
+        Simulator.registerAction(100, () {
+          for (var i = 0; i < topMod.sorted.length; i++) {
+            expect(topMod.sorted[i].value.toInt(), topMod.sorted.length - i);
+          }
+        });
+
+        Simulator.setMaxSimTime(100);
+        // WaveDumper(topMod, outputPath: 'lib/src/sort/recursive_list.vcd');
+
+        await Simulator.run();
       });
 
-      Simulator.setMaxSimTime(100);
-      // WaveDumper(topMod, outputPath: 'lib/src/sort/recursive_list.vcd');
+      test(
+          'should return the sorted results in descending order given '
+          'random seed number with duplicate', () async {
+        const dataWidth = 8;
+        final rand = Random(10);
 
-      await Simulator.run();
+        final clk = SimpleClockGenerator(10).clk;
+        final reset = Logic(name: 'reset');
+
+        var toSortInt = <int>[];
+        final toSort = List<Logic>.generate(pow(2, 3).toInt(), (index) {
+          final number = rand.nextInt(pow(2, 3).toInt() + 1);
+          toSortInt.add(number);
+          return Const(number, width: dataWidth);
+        });
+
+        final topMod = BitonicSort(clk, reset,
+            toSort: toSort, name: 'top_level', isAscending: false);
+        await topMod.build();
+
+        reset.inject(0);
+
+        toSortInt.sort();
+        toSortInt = toSortInt.reversed.toList();
+        Simulator.registerAction(100, () {
+          for (var i = 0; i < topMod.sorted.length; i++) {
+            expect(topMod.sorted[i].value.toInt(), toSortInt[i]);
+          }
+        });
+
+        Simulator.setMaxSimTime(100);
+        WaveDumper(topMod, outputPath: 'test/recursive_list.vcd');
+
+        await Simulator.run();
+      });
+
+      test(
+          'should return the sorted results in descending order given '
+          'random seed number without duplicate', () async {
+        const dataWidth = 8;
+        const seed = 10;
+
+        final listLength = pow(2, 3).toInt();
+        final maxRandNum = listLength;
+
+        final clk = SimpleClockGenerator(10).clk;
+        final reset = Logic(name: 'reset');
+
+        var toSortInt = generateNonDuplicateRand(seed, maxRandNum, listLength);
+
+        final toSort = <Logic>[];
+        for (final num in toSortInt) {
+          toSort.add(Const(num, width: dataWidth));
+        }
+
+        final topMod = BitonicSort(clk, reset,
+            toSort: toSort, name: 'top_level', isAscending: false);
+        await topMod.build();
+
+        reset.inject(0);
+
+        toSortInt.sort();
+        toSortInt = toSortInt.reversed.toList();
+        Simulator.registerAction(100, () {
+          for (var i = 0; i < topMod.sorted.length; i++) {
+            expect(topMod.sorted[i].value.toInt(), toSortInt[i]);
+          }
+        });
+
+        Simulator.setMaxSimTime(100);
+
+        await Simulator.run();
+      });
     });
   });
 }
