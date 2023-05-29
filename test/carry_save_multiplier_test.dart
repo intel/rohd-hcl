e3cb71932d390f5fb0542127674dfb8f29ace573
Yao Jing Quek <yao.jing.quek@intel.com>
2023-05-29 19:16:14 +0800

    Add pipeline test with inputs and outputs await clk

--- test/carry_save_multiplier_test.dart
+++ test/carry_save_multiplier_test.dart
@@ -8,6 +8,9 @@
 // Author: Yao Jing Quek <yao.jing.quek@intel.com>
 //
 
+import 'dart:async';
+import 'dart:math';
+
 import 'package:rohd/rohd.dart';
 import 'package:rohd_hcl/rohd_hcl.dart';
 import 'package:test/test.dart';
@@ -56,5 +59,48 @@ void main() {
     Simulator.setMaxSimTime(150);
 
     await Simulator.run();
+  });
+
+  test('should return correct results when multiply in a pipeline.', () async {
+    // TODO(): do test on width other than 4
+    // TODO(): performs more test
+    // TODO(): Latency formula need to check again
+    final a = Logic(name: 'a', width: 4);
+    final b = Logic(name: 'b', width: 4);
+    final reset = Logic(name: 'reset');
+    final clk = SimpleClockGenerator(10).clk;
+
+    final csm = CarrySaveMultiplier(a, b, clk, reset);
+
+    await csm.build();
+
+    reset.inject(0);
+
+    Simulator.setMaxSimTime(1000);
+    unawaited(Simulator.run());
+
+    Future<void> waitCycles(int numCycles) async {
+      for (var i = 0; i < numCycles; i++) {
+        await clk.nextPosedge;
+      }
+    }
+
+    final inputs = List.generate(
+        10, (index) => List.generate(2, (index) => Random().nextInt(10) + 1));
+
+    for (final input in inputs) {
+      a.put(input[0]);
+      b.put(input[1]);
+
+      await waitCycles(input[0].bitLength + input[1].bitLength).then((value) {
+        print(input[0]);
+        print(input[1]);
+        print(csm.product.value.toInt());
+      });
+
+      await clk.nextNegedge;
+    }
+
+    await Simulator.simulationEnded;
   });
 }
