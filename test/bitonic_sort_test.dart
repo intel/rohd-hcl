d726bdfec0d24ee28c0ab1d6e52462c447131423
Yao Jing Quek <yao.jing.quek@intel.com>
2023-06-08 12:25:11 +0800

    Fix waitcycles to module latency

--- test/bitonic_sort_test.dart
+++ test/bitonic_sort_test.dart
@@ -485,7 +485,7 @@ Future<void> main() async {
           await clk.nextNegedge;
         }
 
-        await waitCycles(inputs.length).then(
+        await waitCycles(topMod.latency).then(
           (value) => {
             Simulator.endSimulation(),
             expect(tested, equals(inputs.length)),
