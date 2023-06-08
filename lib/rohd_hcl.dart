e307d99872879410665454783e0683eeb8fc3b4e
Yao Jing Quek <yao.jing.quek@intel.com>
2023-06-09 01:54:27 +0800

    Carry save multiplier (#28)

--- lib/rohd_hcl.dart
+++ lib/rohd_hcl.dart
@@ -1,11 +1,15 @@
 // Copyright (C) 2023 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 
+export 'src/adder.dart';
 export 'src/arbiter.dart';
+export 'src/carry_save_mutiplier.dart';
 export 'src/exceptions.dart';
 export 'src/fifo.dart';
 export 'src/interfaces/interfaces.dart';
 export 'src/memory.dart';
+export 'src/multiplier.dart';
 export 'src/one_hot.dart';
+export 'src/ripple_carry_adder.dart';
 export 'src/rotate.dart';
 export 'src/sort.dart';
