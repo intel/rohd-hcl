fdf7de5f3dedcb9d47775047b0bc77a7c7004ddb
Quek, Yao Jing <yao.jing.quek@intel.com>
2023-06-21 14:19:08 +0800

    Add the basic functionality

--- doc/confapp_flutter/lib/main.dart
+++ doc/confapp_flutter/lib/main.dart
@@ -1,36 +1,21 @@
 import 'package:confapp_flutter/testingPage.dart';
 import 'package:flutter/material.dart';
 import 'generateRTL.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const ROHDHclConfigApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class ROHDHclConfigApp extends StatelessWidget {
+  const ROHDHclConfigApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'ROHD-HCL',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a blue toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
         colorScheme: ColorScheme.fromSeed(seedColor: const Color(0x00082E8A)),
         useMaterial3: true,
       ),
@@ -39,17 +24,9 @@ class MyApp extends StatelessWidget {
   }
 }
 
+// My Widget
 class SVGeneratorPage extends StatefulWidget {
   const SVGeneratorPage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
 
   final String title;
 
@@ -58,39 +35,31 @@ class SVGeneratorPage extends StatefulWidget {
 }
 
 class _SVGeneratorPageState extends State<SVGeneratorPage> {
-  int _counter = 0;
-  String svTextGen = '';
+  String svTextGen = 'Generated System Verilog here!';
+  String _rotateAmount = '22';
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
-  void _incrementCounter() async {
+  void _generateRTL() async {
+    if (_formKey.currentState!.validate()) {
+      _formKey.currentState!.save();
+
+      print(_rotateAmount);
+    }
     final res = await RotateGenerator().generate();
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-
       svTextGen = res;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    final ButtonStyle btnStyle =
+        ElevatedButton.styleFrom(textStyle: const TextStyle(fontSize: 20));
+    final screenWidth = MediaQuery.of(context).size.width;
+
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
         actions: [
           Row(
@@ -103,53 +72,126 @@ class _SVGeneratorPageState extends State<SVGeneratorPage> {
                   Navigator.push(context,
                       MaterialPageRoute(builder: ((context) => TestPage())));
                 },
-                icon: Icon(Icons.home),
+                icon: const Icon(Icons.home),
               ),
               IconButton(
                 onPressed: () {
                   Navigator.push(context,
                       MaterialPageRoute(builder: ((context) => TestPage())));
                 },
-                icon: Icon(Icons.menu),
+                icon: const Icon(Icons.menu),
               ),
             ],
           )
         ],
       ),
-      body: Container(
-        padding: EdgeInsets.symmetric(horizontal: 16),
-        child: SingleChildScrollView(
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            //
-            // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-            // action in the IDE, or press "p" in the console), to see the
-            // wireframe for each widget.
-            children: <Widget>[
-              const Text(
-                'Generated System Verilog Code:',
+      body: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          // Form
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Card(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Form(
+                    key: _formKey,
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 250,
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'rotate amount',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter value';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _rotateAmount = value ?? '10';
+                              }),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        SizedBox(
+                          width: 250,
+                          child: TextFormField(
+                              decoration: InputDecoration(
+                                border: OutlineInputBorder(),
+                                labelText: 'rotate amount',
+                              ),
+                              validator: (value) {
+                                if (value!.isEmpty) {
+                                  return 'Please enter value';
+                                }
+                                return null;
+                              },
+                              onSaved: (value) {
+                                _rotateAmount = value ?? '10';
+                              }),
+                        ),
+                        SizedBox(
+                          height: 16,
+                        ),
+                        ElevatedButton(
+                          onPressed: _generateRTL,
+                          style: btnStyle,
+                          child: const Text('Generate RTL'),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
               ),
-              Text(
-                svTextGen,
-                style: Theme.of(context).textTheme.headlineMedium,
-              ),
-            ],
+            ),
           ),
-        ),
+
+          // SV output
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Card(
+              child: Container(
+                constraints: BoxConstraints(maxWidth: 600),
+                child: SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Stack(
+                      children: [
+                        Align(
+                          alignment: Alignment.topRight,
+                          child: ElevatedButton(
+                            onPressed: () {
+                              Clipboard.setData(ClipboardData(text: svTextGen));
+                              ScaffoldMessenger.of(context).showSnackBar(
+                                SnackBar(
+                                    content: Text('Text copied to clipboard')),
+                              );
+                            },
+                            child: Text('Copy Text'),
+                          ),
+                        ),
+                        SelectableText(
+                          svTextGen,
+                          style: const TextStyle(
+                            fontSize: 18,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Generate RTL',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
