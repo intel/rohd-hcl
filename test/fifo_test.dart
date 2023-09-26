9ee26d827eeb433f10d09b22979a0493c3213c6b
Max Korbel <max.korbel@intel.com>
2023-09-26 11:58:19 -0700

    remove wavedump code

--- test/fifo_test.dart
+++ test/fifo_test.dart
@@ -570,9 +570,6 @@ void main() {
 
     Directory('tmp_test').createSync();
 
-    // await fifoTest.fifo.build();
-    // WaveDumper(fifoTest.fifo);
-
     final tracker =
         FifoTracker(fifoTest.fifo, outputFolder: 'tmp_test', dumpTable: false);
 
