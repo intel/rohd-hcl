f28cdcd7f83f763d35ffa40bab9f29c9dc33b9bd
aherrera97intl <57770105+aherrera97intl@users.noreply.github.com>
2023-09-28 11:56:09 -0600

    Round Robin Arbiter (#38)

--- lib/src/arbiter.dart
+++ lib/src/arbiter.dart
@@ -59,3 +59,86 @@ class PriorityArbiter extends Arbiter {
     ]);
   }
 }
+
+/// Round Robin Arbiter.
+class RoundRobinArbiter extends Arbiter {
+  /// Mask to define pending requests to be attended
+  /// the main idea is to mask the requests as they are granted to ensure all
+  /// requests are granted once before resetting the mask
+  /// example:
+  /// [clk] [_requests] [_requestMask]   [_grantMask]   [_grants]
+  ///  0    00100010   &  11111111      =   00100010  ->  00000010
+  ///  1    00100010   &  11111100      =   00100000  ->  00100000
+  ///  2    10000001   &  11000000      =   10000000  ->  10000000
+  /// On clock 0 and 1 the grants are used to generate the
+  /// requestMask, therefore grantMask changes to grant the next request
+  /// Clock 3 shows when a request changes, the requestMask make sure to
+  /// handle the missing requests before resetting
+  List<Logic> _requestMask = [];
+
+  /// Result of masking [_requests] with [_requestMask]
+  /// Contains bits of [_requests] but as bits are granted, will turn off
+  List<Logic> _grantMask = [];
+
+  /// Round Robin arbiter handles requests by granting each requestor
+  /// and keeping record of requests already granted, in order to mask it until
+  /// granting the turn of each request to start again
+  RoundRobinArbiter(super.requests,
+      {required Logic clk, required Logic reset}) {
+    clk = addInput('clk', clk);
+    reset = addInput('reset', reset);
+    _requestMask = List.generate(count, (i) => Logic(name: 'requestMask$i'));
+    _grantMask = List.generate(count, (i) => Logic(name: 'grantMask$i'));
+    Sequential(clk, [
+      If(reset, then: [
+        for (var g = 0; g < count; g++) _requestMask[g] < 1,
+      ], orElse: [
+        // Use [_grants] to turn the bit granted to 0 along with all bits
+        // to the right to avoid, in case if  [_requests] changes, granting a
+        // previous request
+        // Example:
+        // [_grants] [requestMask]
+        // 00001000   11110000
+        Case(_grants.rswizzle(), conditionalType: ConditionalType.unique, [
+          for (var i = 0; i < count; i++)
+            CaseItem(
+                Const(LogicValue.filled(count, LogicValue.zero)
+                    .withSet(i, LogicValue.one)),
+                [
+                  for (var g = 0; g < count; g++)
+                    _requestMask[g] < (i < g ? 1 : 0),
+                ])
+          // In case [_grants] are all 0s, requestMask gets a reset
+        ], defaultItem: [
+          for (var g = 0; g < count; g++) _requestMask[g] < 1,
+        ])
+      ])
+    ]);
+    Combinational([
+      for (var g = 0; g < count; g++)
+        _grantMask[g] < _requestMask[g] & _requests[g],
+      // CaseZ uses [_grantMask] to set the [_grants] based on the
+      // least significant bit
+      CaseZ(
+        _grantMask.rswizzle(), conditionalType: ConditionalType.priority,
+        [
+          for (var i = 0; i < count; i++)
+            CaseItem(
+                Const(LogicValue.filled(count, LogicValue.z)
+                    .withSet(i, LogicValue.one)),
+                [
+                  for (var g = 0; g < count; g++) _grants[g] < (i == g ? 1 : 0),
+                ]),
+        ],
+        // When all bits have been granted, [_grantMask] turns all to 0s because
+        // of the [_requestMask], therefore logic to define next [_grants]
+        // relies on [_request] least significant bit
+        defaultItem: [
+          for (var g = 0; g < count; g++)
+            _grants[g] <
+                ~_requests.rswizzle().getRange(0, g).or() & _requests[g],
+        ],
+      ),
+    ]);
+  }
+}
--- test/arbiter_test.dart
+++ test/arbiter_test.dart
@@ -7,11 +7,15 @@
 // 2023 March 13
 // Author: Max Korbel <max.korbel@intel.com>
 
+import 'dart:async';
 import 'package:rohd/rohd.dart';
 import 'package:rohd_hcl/rohd_hcl.dart';
 import 'package:test/test.dart';
 
 void main() {
+  tearDown(() async {
+    await Simulator.reset();
+  });
   test('priority arbiter', () async {
     const width = 8;
 
@@ -33,5 +37,130 @@ void main() {
 
     vector.put(bin('00010100'));
     expect(grantVec.value, LogicValue.ofString('00000100'));
+  });
+  test('round robin logic', () async {
+    final clk = SimpleClockGenerator(10).clk;
+    const width = 8;
+    final vector = Logic(width: width);
+    final reset = Logic();
+    final reqs = List.generate(width, (i) => vector[i]);
+    final arb = RoundRobinArbiter(reqs, clk: clk, reset: reset);
+    final grants = arb.grants.rswizzle();
+    await arb.build();
+    WaveDumper(arb);
+    unawaited(Simulator.run());
+
+    vector.put(bin('01001001'));
+    reset.put(1); //Reset arbiter
+    await clk.nextNegedge;
+    await clk.nextNegedge;
+    reset.put(0);
+    expect(grants.value, LogicValue.ofString('00000001'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00001000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('01000000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00000001'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00001000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('01000000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00000001'));
+    await clk.nextNegedge;
+    vector.put(bin('00000000'));
+    expect(grants.value, LogicValue.ofString('00000000'));
+    await clk.nextNegedge;
+    vector.put(bin('11001010'));
+    expect(grants.value, LogicValue.ofString('00000010'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00001000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('01000000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('10000000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00000010'));
+
+    Simulator.endSimulation();
+    await Simulator.simulationEnded;
+  });
+
+  test('Round Robin dynamic request test', () async {
+    final clk = SimpleClockGenerator(10).clk;
+    const width = 8;
+    final vector = Logic(width: width);
+    final reset = Logic();
+    final reqs = List.generate(width, (i) => vector[i]);
+    final arb = RoundRobinArbiter(reqs, clk: clk, reset: reset);
+    final grants = arb.grants.rswizzle();
+    await arb.build();
+    unawaited(Simulator.run());
+    WaveDumper(arb);
+
+    reset.put(1); //Reset arbiter
+    await clk.nextNegedge;
+    await clk.nextNegedge;
+    reset.put(0);
+    vector.put(bin('01000111'));
+    expect(grants.value, LogicValue.ofString('00000001'));
+    await clk.nextNegedge;
+    vector.put(bin('10000101'));
+    expect(grants.value, LogicValue.ofString('00000100'));
+    await clk.nextNegedge;
+    vector.put(bin('01001000'));
+    expect(grants.value, LogicValue.ofString('00001000'));
+    await clk.nextNegedge;
+    vector.put(bin('10010001'));
+    expect(grants.value, LogicValue.ofString('00010000'));
+    await clk.nextNegedge;
+    vector.put(bin('10000000'));
+    expect(grants.value, LogicValue.ofString('10000000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('10000000'));
+    vector.put(bin('10000000'));
+    expect(grants.value, LogicValue.ofString('10000000'));
+    await clk.nextNegedge;
+    vector.put(bin('10010001'));
+    expect(grants.value, LogicValue.ofString('00000001'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00010000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('10000000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00000001'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('00010000'));
+    await clk.nextNegedge;
+    expect(grants.value, LogicValue.ofString('10000000'));
+
+    Simulator.endSimulation();
+    await Simulator.simulationEnded;
+  });
+
+  test('all reqs', () async {
+    final clk = SimpleClockGenerator(10).clk;
+    final reset = Logic();
+    final requests = List.generate(8, (index) => Const(1));
+    final arbiter = RoundRobinArbiter(requests, clk: clk, reset: reset);
+    await arbiter.build();
+    Simulator.setMaxSimTime(5000);
+    unawaited(Simulator.run());
+    WaveDumper(arbiter);
+
+    reset.inject(0);
+    await clk.nextNegedge;
+    reset.inject(1);
+    await clk.nextNegedge;
+    await clk.nextNegedge;
+    reset.inject(0);
+    for (var i = 0; i < 30; i++) {
+      expect(arbiter.grants.rswizzle().value.toInt(), 1 << (i % 8));
+      await clk.nextNegedge;
+    }
+
+    Simulator.endSimulation();
+    await Simulator.simulationEnded;
   });
 }
