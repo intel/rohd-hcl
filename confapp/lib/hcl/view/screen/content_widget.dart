3b9b46f77203e1e965f08995142c0fb656c9fa87
Max Korbel <max.korbel@intel.com>
2023-12-04 14:09:59 -0800

    generate and show json config info

--- confapp/lib/hcl/view/screen/content_widget.dart
+++ confapp/lib/hcl/view/screen/content_widget.dart
@@ -70,8 +70,12 @@ class _SVGeneratorState extends State<SVGenerator> {
           if (knob is IntConfigKnob) FilteringTextInputFormatter.digitsOnly,
           FilteringTextInputFormatter.singleLineFormatter,
         ],
-        onSaved: (value) {
+        onChanged: (value) {
           setState(() {
+            if (value.isEmpty) {
+              return;
+            }
+
             if (knob is IntConfigKnob) {
               knob.value = int.parse(value.toString());
             } else {
@@ -118,6 +122,128 @@ class _SVGeneratorState extends State<SVGenerator> {
     return SizedBox(width: 400, child: selector);
   }
 
+  Widget _generateJsonCard(
+    double screenHeight,
+    double screenWidth,
+  ) {
+    return Card(
+      child: Container(
+        constraints: BoxConstraints(
+            maxHeight: screenHeight / 3, maxWidth: screenWidth / 3),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: BlocBuilder<ComponentCubit, Configurator>(
+                builder: (context, component) {
+              final jsonCode = component.toJson(pretty: true);
+              return Column(
+                children: [
+                  _copyAndDownloadButtons(
+                      isLoading: false,
+                      code: jsonCode,
+                      fileName: '${component.name}.config.json'),
+                  Align(
+                    alignment: Alignment.centerLeft,
+                    child: SelectableText(
+                      jsonCode,
+                      style: GoogleFonts.robotoMono(
+                        fontSize: 12,
+                      ),
+                    ),
+                  )
+                ],
+              );
+            }),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _copyAndDownloadButtons(
+      {required bool isLoading,
+      required String code,
+      required String fileName,
+      int maxChars = 10000}) {
+    final tooBig = code.length > maxChars;
+
+    return Align(
+        alignment: Alignment.topRight,
+        child: isLoading
+            ? const CircularProgressIndicator()
+            : SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(mainAxisAlignment: MainAxisAlignment.end, children: [
+                  ElevatedButton(
+                    onPressed: tooBig
+                        ? null
+                        : () {
+                            Clipboard.setData(ClipboardData(text: code));
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                  content: Text('Text copied to clipboard')),
+                            );
+                          },
+                    child: tooBig
+                        ? const Text('Copy (too large)')
+                        : const Text('Copy'),
+                  ),
+                  ElevatedButton(
+                    onPressed: () {
+                      final bytes = base64Encode(code.codeUnits);
+                      final uri = 'data:application/octet-stream;base64,$bytes';
+                      AnchorElement(href: uri)
+                        ..setAttribute('download', fileName)
+                        ..click();
+                    },
+                    child: const Text('Download'),
+                  ),
+                ])));
+  }
+
+  Widget _generatedRtlCard(double screenHeight, double screenWidth) {
+    return Card(
+      child: Container(
+        constraints: BoxConstraints(
+            maxHeight: screenHeight * 0.8, maxWidth: screenWidth / 3),
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: BlocBuilder<SystemVerilogCubit, SystemVerilogCubitState>(
+              builder: (context, state) {
+                final svCode = state.systemVerilog;
+                const maxChars = 10000;
+                final tooBig = svCode.length > maxChars;
+                return Column(
+                  children: [
+                    if (state.generationState != GenerationState.initial)
+                      _copyAndDownloadButtons(
+                          isLoading:
+                              state.generationState == GenerationState.loading,
+                          code: svCode,
+                          fileName: '${state.name}.sv'),
+                    Align(
+                      alignment: Alignment.centerLeft,
+                      child: SelectableText(
+                        tooBig
+                            ? '${svCode.substring(0, maxChars)}'
+                                '\n...too long to show'
+                            : svCode,
+                        style: GoogleFonts.robotoMono(
+                          fontSize: 12,
+                        ),
+                      ),
+                    )
+                  ],
+                );
+              },
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     final rtlCubit = context.read<SystemVerilogCubit>();
@@ -127,167 +253,99 @@ class _SVGeneratorState extends State<SVGenerator> {
     return Row(
       mainAxisAlignment: MainAxisAlignment.spaceEvenly,
       children: [
-        BlocBuilder<ComponentCubit, Configurator>(
-          builder: (context, component) {
-            final textFormField = [];
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            BlocBuilder<ComponentCubit, Configurator>(
+              builder: (context, component) {
+                final textFormField = [];
 
-            // Add a title
-            textFormField.add(
-              Text(
-                component.name,
-                style: const TextStyle(
-                  fontSize: 25,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-            );
-            for (var knobEntry in component.knobs.entries) {
-              final knob = knobEntry.value;
-              final knobLabel = knobEntry.key;
-
-              textFormField.add(
-                const SizedBox(
-                  height: 16,
-                ),
-              );
-
-              textFormField.add(
-                _generateKnobControl(knobLabel, knob),
-              );
-            }
-
-            return Container(
-              margin: const EdgeInsets.all(10),
-              constraints: BoxConstraints(
-                maxHeight: screenHeight / 1.2,
-                maxWidth: screenWidth / 3,
-              ),
-              child: Card(
-                child: Padding(
-                  padding: const EdgeInsets.all(16.0),
-                  child: Form(
-                    key: _formKey,
-                    child: Column(
-                      mainAxisSize: MainAxisSize.min,
-                      children: [
-                        ...textFormField,
-                        const SizedBox(
-                          height: 16,
-                        ),
-                        ElevatedButton(
-                          key: const Key('generateRTL'),
-                          onPressed: () async {
-                            try {
-                              rtlCubit.setLoading();
-
-                              // allow some time for loading spinner to appear
-                              await Future.delayed(
-                                  const Duration(milliseconds: 10));
-
-                              final rtlRes = await _generateRTL(component);
-
-                              rtlCubit.setRTL(rtlRes, component.sanitaryName);
-                            } on Exception catch (e) {
-                              var message = e.toString();
-                              if (e is RohdHclException) {
-                                message = e.message;
-                              }
-                              rtlCubit.setRTL(
-                                  'Error generating:\n\n$message', 'error');
-                            }
-                          },
-                          style: btnStyle,
-                          child: const Text('Generate RTL'),
-                        ),
-                      ],
+                // Add a title
+                textFormField.add(
+                  Text(
+                    component.name,
+                    style: const TextStyle(
+                      fontSize: 25,
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                ),
-              ),
-            );
-          },
-        ),
-        Expanded(
-            child: Card(
-          child: Container(
-            constraints: BoxConstraints(
-                maxHeight: screenHeight / 2, maxWidth: screenWidth / 3),
-            child: SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: BlocBuilder<SystemVerilogCubit, SystemVerilogCubitState>(
-                  builder: (context, state) {
-                    final svCode = state.systemVerilog;
-                    const maxChars = 10000;
-                    final tooBig = svCode.length > maxChars;
-                    return Column(
-                      children: [
-                        if (state.generationState != GenerationState.initial)
-                          Align(
-                              alignment: Alignment.topRight,
-                              child: state.generationState ==
-                                      GenerationState.loading
-                                  ? const CircularProgressIndicator()
-                                  : SingleChildScrollView(
-                                      scrollDirection: Axis.horizontal,
-                                      child: Row(
-                                          mainAxisAlignment:
-                                              MainAxisAlignment.end,
-                                          children: [
-                                            ElevatedButton(
-                                              onPressed: tooBig
-                                                  ? null
-                                                  : () {
-                                                      Clipboard.setData(
-                                                          ClipboardData(
-                                                              text: svCode));
-                                                      ScaffoldMessenger.of(
-                                                              context)
-                                                          .showSnackBar(
-                                                        const SnackBar(
-                                                            content: Text(
-                                                                'Text copied to clipboard')),
-                                                      );
-                                                    },
-                                              child: tooBig
-                                                  ? const Text(
-                                                      'Copy SV (too large)')
-                                                  : const Text('Copy SV'),
-                                            ),
-                                            ElevatedButton(
-                                              onPressed: () {
-                                                final bytes = base64Encode(
-                                                    svCode.codeUnits);
-                                                final uri =
-                                                    'data:application/octet-stream;base64,$bytes';
-                                                AnchorElement(href: uri)
-                                                  ..setAttribute('download',
-                                                      '${state.name}.sv')
-                                                  ..click();
-                                              },
-                                              child: const Text('Download SV'),
-                                            ),
-                                          ]))),
-                        Align(
-                          alignment: Alignment.centerLeft,
-                          child: SelectableText(
-                            tooBig
-                                ? '${svCode.substring(0, maxChars)}'
-                                    '\n...too long to show'
-                                : svCode,
-                            style: GoogleFonts.robotoMono(
-                              fontSize: 12,
+                );
+                for (var knobEntry in component.knobs.entries) {
+                  final knob = knobEntry.value;
+                  final knobLabel = knobEntry.key;
+
+                  textFormField.add(
+                    const SizedBox(
+                      height: 16,
+                    ),
+                  );
+
+                  textFormField.add(
+                    _generateKnobControl(knobLabel, knob),
+                  );
+                }
+
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  constraints: BoxConstraints(
+                    maxHeight: screenHeight * 2 / 3,
+                    maxWidth: screenWidth / 3,
+                  ),
+                  child: Card(
+                    child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Form(
+                        key: _formKey,
+                        child: Column(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            ...textFormField,
+                            const SizedBox(
+                              height: 16,
                             ),
-                          ),
-                        )
-                      ],
-                    );
-                  },
-                ),
-              ),
+                            ElevatedButton(
+                              key: const Key('generateRTL'),
+                              onPressed: () async {
+                                try {
+                                  rtlCubit.setLoading();
+
+                                  // allow some time for loading spinner to appear
+                                  await Future.delayed(
+                                      const Duration(milliseconds: 10));
+
+                                  final rtlRes = await _generateRTL(component);
+
+                                  rtlCubit.setRTL(
+                                      rtlRes, component.sanitaryName);
+                                } on Exception catch (e) {
+                                  var message = e.toString();
+                                  if (e is RohdHclException) {
+                                    message = e.message;
+                                  }
+                                  rtlCubit.setRTL(
+                                      'Error generating:\n\n$message', 'error');
+                                }
+                              },
+                              style: btnStyle,
+                              child: const Text('Generate RTL'),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+                );
+              },
             ),
-          ),
-        )),
+            _generateJsonCard(screenHeight, screenWidth),
+          ],
+        ),
+        Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            _generatedRtlCard(screenHeight, screenWidth),
+          ],
+        ),
       ],
     );
   }
