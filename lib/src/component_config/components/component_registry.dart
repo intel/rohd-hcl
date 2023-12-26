9834e74eb46091f613e64d9ef021b52d80928e8f
Max Korbel <max.korbel@intel.com>
2023-12-26 10:57:26 -0800

    fix lint

--- lib/src/component_config/components/component_registry.dart
+++ lib/src/component_config/components/component_registry.dart
@@ -8,7 +8,6 @@
 // Author: Max Korbel <max.korbel@intel.com>
 
 import 'package:rohd_hcl/rohd_hcl.dart';
-import 'package:rohd_hcl/src/component_config/components/config_round_robin_arbiter.dart';
 
 /// A list of [Configurator]s for ROHD-HCL components.
 List<Configurator> get componentRegistry => [
