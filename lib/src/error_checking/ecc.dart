ef3d331bbc378c9f7c2a1d4ab288941c461f45fd
Max Korbel <max.korbel@intel.com>
2024-01-17 09:39:27 -0800

    first impl of receiver

--- lib/src/error_checking/ecc.dart
+++ lib/src/error_checking/ecc.dart
@@ -1,12 +1,24 @@
+import 'dart:math';
+
 import 'package:rohd/rohd.dart';
 import 'package:rohd/src/signals/signals.dart';
 import 'package:rohd_hcl/rohd_hcl.dart';
 
+bool _isPowerOfTwo(int n) => n != 0 && (n & (n - 1) == 0);
+
 class HammingEccTransmitter extends ErrorCheckingTransmitter {
   HammingEccTransmitter(super.data, {super.name = 'hamming_ecc_tx'})
-      : super(codeWidth: log2Ceil(data.width));
+      : super(codeWidth: _parityBitsRequired(data.width));
 
-  bool _isPowerOfTwo(int n) => n != 0 && (n & (n - 1) == 0);
+  static int _parityBitsRequired(int dataWidth) {
+    var m = 0;
+    double k;
+    do {
+      m++;
+      k = pow(2, m) - m - 1;
+    } while (k < dataWidth);
+    return m;
+  }
 
   @override
   Logic calculateCode() {
@@ -37,4 +49,54 @@ class HammingEccTransmitter extends ErrorCheckingTransmitter {
         Logic(name: 'p${1 << i}')..gets(parityBits[i]!),
     ].rswizzle();
   }
+}
+
+class HammingEccReceiver extends ErrorCheckingReceiver {
+  static int _codeWidthFromBusWidth(int busWidth) => log2Ceil(busWidth + 1);
+
+  Map<int, int> get _encodingToData {
+    final mapping = <int, int>{};
+    var dataIdx = 0;
+    for (var encodedIdx = 0; encodedIdx <= bus.width; encodedIdx++) {
+      if (!_isPowerOfTwo(encodedIdx)) {
+        mapping[encodedIdx] = dataIdx++;
+      }
+    }
+    return mapping;
+  }
+
+  ///TODO
+  HammingEccReceiver(super.bus, {super.name = 'hamming_ecc_rx'})
+      : super(
+            codeWidth: _codeWidthFromBusWidth(bus.width),
+            supportsErrorCorrection: true) {
+    _syndrome <= code ^ HammingEccTransmitter(originalData).code;
+
+    final correction = Logic(name: 'correction', width: bus.width)
+      ..gets(mux(
+        error,
+        BinaryToOneHot(_syndrome - 1).encoded.getRange(0, bus.width),
+        Const(0, width: bus.width),
+      ));
+
+    _correctedData <=
+        [
+          for (var i = 0; i < bus.width; i++)
+            if (_encodingToData.containsKey(i))
+              Logic(name: 'd${_encodingToData[i]! + 1}')
+                ..gets(originalData[_encodingToData[i]] ^ correction[i])
+        ].rswizzle();
+  }
+
+  late final Logic _correctedData = Logic(width: originalData.width);
+  late final Logic _syndrome = Logic(name: 'syndrome', width: code.width);
+
+  @override
+  Logic calculateCorrectableError() => _syndrome.or(); //TODO
+
+  @override
+  Logic? calculateCorrectedData() => _correctedData;
+
+  @override
+  Logic calculateUncorrectableError() => Const(0); //TODO
 }
--- test/ecc_test.dart
+++ test/ecc_test.dart
@@ -3,8 +3,14 @@ import 'package:rohd_hcl/src/error_checking/ecc.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ecc test 1', () async {
+  test('ecc tx', () async {
     final mod = HammingEccTransmitter(Logic(width: 15));
+    await mod.build();
+    print(mod.generateSynth());
+  });
+
+  test('ecc rx', () async {
+    final mod = HammingEccReceiver(Logic(width: 20));
     await mod.build();
     print(mod.generateSynth());
   });
