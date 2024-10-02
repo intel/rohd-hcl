ca6cac114132bd2a6991c4d157062659b406b89a
anton.a.sorokin <aasoroki@plxcjh049.pdx.intel.com>
2024-10-01 20:42:40 -0700

    Compound added extend Adder

--- lib/src/arithmetic/compound_adder.dart
+++ lib/src/arithmetic/compound_adder.dart
@@ -13,17 +13,7 @@ import 'package:rohd/rohd.dart';
 import 'package:rohd_hcl/rohd_hcl.dart';
 
 /// An abstract class for all compound adder module.
-abstract class CompoundAdder extends Module {
-  /// The input to the adder pin [a].
-  @protected
-  Logic get a => input('a');
-
-  /// The input to the adder pin [b].
-  @protected
-  Logic get b => input('b');
-
-  /// The addition results in 2s complement form as [sum]
-  Logic get sum => output('sum');
+abstract class CompoundAdder extends Adder {
 
   /// The addition results (+1) in 2s complement form as [sum1]
   Logic get sum1 => output('sum1');
@@ -31,13 +21,10 @@ abstract class CompoundAdder extends Module {
   /// Takes in input [a] and input [b] and return the [sum] of the addition
   /// result and [sum1] sum + 1. 
   /// The width of input [a] and [b] must be the same.
-  CompoundAdder(Logic a, Logic b, {super.name}) {
+  CompoundAdder(super.a, super.b, {super.name}) {
     if (a.width != b.width) {
       throw RohdHclException('inputs of a and b should have same width.');
     }
-    addInput('a', a, width: a.width);
-    addInput('b', b, width: b.width);
-    addOutput('sum', width: a.width + 1);
     addOutput('sum1', width: a.width + 1);
   }
 }
