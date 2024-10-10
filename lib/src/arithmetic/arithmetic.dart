9a2cf5b86f136dfcf5d1907439cf2a6a3cc95694
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2024-10-09 17:39:23 -0700

    add utils to exports

--- lib/src/arithmetic/arithmetic.dart
+++ lib/src/arithmetic/arithmetic.dart
@@ -2,6 +2,7 @@
 // SPDX-License-Identifier: BSD-3-Clause
 
 export 'adder.dart';
+export 'arithmetic_utils.dart';
 export 'carry_save_mutiplier.dart';
 export 'divider.dart';
 export 'floating_point/floating_point.dart';
