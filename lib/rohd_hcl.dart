e7a24f7f93ebc028912503eee9c37c274ee60b9d
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2025-01-12 13:54:35 -0800

    forgot to saveall in vscode -- rohd_hcl.dart fix

--- lib/rohd_hcl.dart
+++ lib/rohd_hcl.dart
@@ -4,7 +4,6 @@
 export 'src/arbiters/arbiters.dart';
 export 'src/arithmetic/arithmetic.dart';
 export 'src/binary_gray.dart';
-export 'src/reduction_tree.dart';
 export 'src/clock_gating.dart';
 export 'src/component_config/component_config.dart';
 export 'src/count.dart';
@@ -18,6 +17,7 @@ export 'src/find.dart';
 export 'src/interfaces/interfaces.dart';
 export 'src/memory/memories.dart';
 export 'src/models/models.dart';
+export 'src/reduction_tree.dart';
 export 'src/rotate.dart';
 export 'src/serialization/serialization.dart';
 export 'src/shift_register.dart';
