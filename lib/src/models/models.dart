ca671b9492fb584f5899c5bdc436dfd460fc5b86
Josh Kimmel <joshua1.kimmel@intel.com>
2025-02-04 17:31:37 -0800

    Adding export of axi4 bfm.

--- lib/src/models/models.dart
+++ lib/src/models/models.dart
@@ -1,7 +1,8 @@
-// Copyright (C) 2023-2024 Intel Corporation
+// Copyright (C) 2024-2025 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 
 export 'apb_bfm/apb_bfm.dart';
+export 'axi4_bfm/axi4_bfm.dart';
 export 'memory_model.dart';
 export 'ready_valid_bfm/ready_valid_bfm.dart';
 export 'sparse_memory_storage.dart';
