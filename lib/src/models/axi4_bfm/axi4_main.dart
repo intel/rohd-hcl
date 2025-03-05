d4d5b03c8adb8b950e3353c66aaa413c155db6b1
Josh Kimmel <joshua1.kimmel@intel.com>
2025-03-04 17:06:51 -0800

    Addressing more feedback.

--- lib/src/models/axi4_bfm/axi4_main.dart
+++ lib/src/models/axi4_bfm/axi4_main.dart
@@ -39,11 +39,128 @@ class Axi4Channel {
     this.rIntf,
     this.wIntf,
   })  : hasRead = rIntf != null,
-        hasWrite = wIntf != null,
-        assert(
-            rIntf != null || wIntf != null,
-            'A channel must support either'
-            ' reads or writes (or both)');
+        hasWrite = wIntf != null {
+    if (rIntf == null && wIntf == null) {
+      throw RohdHclException('A channel must support either'
+          ' reads or writes (or both)');
+    }
+  }
+}
+
+/// An agent for sending and monitoring read requests
+///
+/// Driven read packets will update the returned data into the same packet.
+class Axi4ReadAgent extends Agent {
+  /// AXI4 System Interface.
+  final Axi4SystemInterface sIntf;
+
+  /// Channel that the agent can send requests on.
+  final Axi4Channel channel;
+
+  /// The sequencer where read requests should be sent.
+  late final Sequencer<Axi4ReadRequestPacket> sequencer;
+
+  /// The driver that sends read requests over the interface.
+  late final Axi4ReadMainDriver driver;
+
+  /// Monitoring of read requests over the interface.
+  late final Axi4ReadMonitor monitor;
+
+  /// The number of cycles before timing out if no transactions can be sent.
+  final int timeoutCycles;
+
+  /// The number of cycles before an objection will be dropped when there are
+  /// no pending packets to send.
+  final int dropDelayCycles;
+
+  /// Constructs a new [Axi4ReadAgent].
+  Axi4ReadAgent({
+    required this.sIntf,
+    required this.channel,
+    required Component parent,
+    String name = 'axiReadAgent',
+    this.timeoutCycles = 500,
+    this.dropDelayCycles = 30,
+  }) : super(name, parent) {
+    if (!channel.hasRead) {
+      throw RohdHclException('A read agent must be associated with a channel '
+          'that can send read requests.');
+    }
+
+    sequencer =
+        Sequencer<Axi4ReadRequestPacket>('${name}_axiRdSequencer', this);
+    driver = Axi4ReadMainDriver(
+      parent: this,
+      sIntf: sIntf,
+      rIntf: channel.rIntf!,
+      sequencer: sequencer,
+      timeoutCycles: timeoutCycles,
+      dropDelayCycles: dropDelayCycles,
+      name: '${name}_axiRdDriver',
+    );
+    monitor = Axi4ReadMonitor(
+        sIntf: sIntf,
+        rIntf: channel.rIntf!,
+        parent: parent,
+        name: '${name}_axiRdMonitor');
+  }
+}
+
+/// An agent for sending and monitoring write requests.
+class Axi4WriteAgent extends Agent {
+  /// AXI4 System Interface.
+  final Axi4SystemInterface sIntf;
+
+  /// Channel that the agent can send requests on.
+  final Axi4Channel channel;
+
+  /// The sequencer where write requests should be sent.
+  late final Sequencer<Axi4WriteRequestPacket> sequencer;
+
+  /// The driver that sends write requests over the interface.
+  late final Axi4WriteMainDriver driver;
+
+  /// Monitoring of write requests over the interface.
+  late final Axi4WriteMonitor monitor;
+
+  /// The number of cycles before timing out if no transactions can be sent.
+  final int timeoutCycles;
+
+  /// The number of cycles before an objection will be dropped when there are
+  /// no pending packets to send.
+  final int dropDelayCycles;
+
+  /// Constructs a new [Axi4WriteAgent].
+  Axi4WriteAgent({
+    required this.sIntf,
+    required this.channel,
+    required Component parent,
+    String name = 'axiWriteAgent',
+    this.timeoutCycles = 500,
+    this.dropDelayCycles = 30,
+  }) : super(name, parent) {
+    if (!channel.hasWrite) {
+      throw RohdHclException('A write agent must be associated with a channel '
+          'that can send write requests.');
+    }
+
+    sequencer =
+        Sequencer<Axi4WriteRequestPacket>('${name}_axiRdSequencer', this);
+    driver = Axi4WriteMainDriver(
+      parent: this,
+      sIntf: sIntf,
+      wIntf: channel.wIntf!,
+      sequencer: sequencer,
+      timeoutCycles: timeoutCycles,
+      dropDelayCycles: dropDelayCycles,
+      name: '${name}_axiWrDriver',
+    );
+    monitor = Axi4WriteMonitor(
+        sIntf: sIntf,
+        wIntf: channel.wIntf!,
+        parent: parent,
+        name: '${name}_axiWrMonitor');
+  }
 }
 
 /// An agent for sending requests on
@@ -57,23 +174,11 @@ class Axi4MainAgent extends Agent {
   /// Channels that the agent can send requests on.
   final List<Axi4Channel> channels;
 
-  /// The sequencers where read requests should be sent.
-  final List<Sequencer<Axi4ReadRequestPacket>> rdSequencers = [];
+  /// Agents to manage individual read channels.
+  final List<Axi4ReadAgent> rdAgents = [];
 
-  /// The sequencers where write requests should be sent.
-  final List<Sequencer<Axi4WriteRequestPacket>> wrSequencers = [];
-
-  /// The drivers that send read requests over the interface.
-  final List<Axi4ReadMainDriver> rdDrivers = [];
-
-  /// The drivers that send write requests over the interface.
-  final List<Axi4WriteMainDriver> wrDrivers = [];
-
-  /// Monitoring of read requests over the interface.
-  final List<Axi4ReadMonitor> rdMonitors = [];
-
-  /// Monitoring of write requests over the interface.
-  final List<Axi4WriteMonitor> wrMonitors = [];
+  /// Agents to manage individual write channels.
+  final List<Axi4WriteAgent> wrAgents = [];
 
   /// The number of cycles before timing out if no transactions can be sent.
   final int timeoutCycles;
@@ -89,37 +194,37 @@ class Axi4MainAgent extends Agent {
   /// Get the ith channel's read sequencer.
   Sequencer<Axi4ReadRequestPacket>? getRdSequencer(int channelId) =>
       _readAddrToChannel.containsKey(channelId)
-          ? rdSequencers[_readAddrToChannel[channelId]!]
+          ? rdAgents[_readAddrToChannel[channelId]!].sequencer
           : null;
 
   /// Get the ith channel's write sequencer.
   Sequencer<Axi4WriteRequestPacket>? getWrSequencer(int channelId) =>
       _writeAddrToChannel.containsKey(channelId)
-          ? wrSequencers[_writeAddrToChannel[channelId]!]
+          ? wrAgents[_writeAddrToChannel[channelId]!].sequencer
           : null;
 
   /// Get the ith channel's read driver.
   Axi4ReadMainDriver? getRdDriver(int channelId) =>
       _readAddrToChannel.containsKey(channelId)
-          ? rdDrivers[_readAddrToChannel[channelId]!]
+          ? rdAgents[_readAddrToChannel[channelId]!].driver
           : null;
 
   /// Get the ith channel's write driver.
   Axi4WriteMainDriver? getWrDriver(int channelId) =>
       _writeAddrToChannel.containsKey(channelId)
-          ? wrDrivers[_writeAddrToChannel[channelId]!]
+          ? wrAgents[_writeAddrToChannel[channelId]!].driver
           : null;
 
   /// Get the ith channel's read monitor.
   Axi4ReadMonitor? getRdMonitor(int channelId) =>
       _readAddrToChannel.containsKey(channelId)
-          ? rdMonitors[_readAddrToChannel[channelId]!]
+          ? rdAgents[_readAddrToChannel[channelId]!].monitor
           : null;
 
   /// Get the ith channel's write monitor.
   Axi4WriteMonitor? getWrMonitor(int channelId) =>
       _writeAddrToChannel.containsKey(channelId)
-          ? wrMonitors[_writeAddrToChannel[channelId]!]
+          ? wrAgents[_writeAddrToChannel[channelId]!].monitor
           : null;
 
   /// Constructs a new [Axi4MainAgent].
@@ -133,43 +238,149 @@ class Axi4MainAgent extends Agent {
   }) : super(name, parent) {
     for (var i = 0; i < channels.length; i++) {
       if (channels[i].hasRead) {
-        rdSequencers.add(Sequencer<Axi4ReadRequestPacket>(
-            'axiRdSequencer${channels[i].channelId}', this));
-        _readAddrToChannel[i] = rdSequencers.length - 1;
-        rdDrivers.add(Axi4ReadMainDriver(
-          parent: this,
-          sIntf: sIntf,
-          rIntf: channels[i].rIntf!,
-          sequencer: rdSequencers[_readAddrToChannel[i]!],
-          timeoutCycles: timeoutCycles,
-          dropDelayCycles: dropDelayCycles,
-          name: 'axiRdDriver${channels[i].channelId}',
-        ));
-        rdMonitors.add(Axi4ReadMonitor(
+        rdAgents.add(Axi4ReadAgent(
             sIntf: sIntf,
-            rIntf: channels[i].rIntf!,
+            channel: channels[i],
             parent: parent,
-            name: 'axiRdMonitor${channels[i].channelId}'));
+            name: 'axi4RdAgent_$i'));
+        _readAddrToChannel[i] = rdAgents.length - 1;
       }
       if (channels[i].hasWrite) {
-        wrSequencers.add(Sequencer<Axi4WriteRequestPacket>(
-            'axiWrSequencer${channels[i].channelId}', this));
-        _writeAddrToChannel[i] = wrSequencers.length - 1;
-        wrDrivers.add(Axi4WriteMainDriver(
-          parent: this,
-          sIntf: sIntf,
-          wIntf: channels[i].wIntf!,
-          sequencer: wrSequencers[_writeAddrToChannel[i]!],
-          timeoutCycles: timeoutCycles,
-          dropDelayCycles: dropDelayCycles,
-          name: 'axiWrDriver${channels[i].channelId}',
-        ));
-        wrMonitors.add(Axi4WriteMonitor(
+        wrAgents.add(Axi4WriteAgent(
             sIntf: sIntf,
-            wIntf: channels[i].wIntf!,
+            channel: channels[i],
             parent: parent,
-            name: 'axiWrMonitor${channels[i].channelId}'));
+            name: 'axi4WrAgent_$i'));
+        _writeAddrToChannel[i] = wrAgents.length - 1;
       }
     }
   }
 }
+
+// /// An agent for sending requests on
+// /// [Axi4ReadInterface]s and [Axi4WriteInterface]s.
+// ///
+// /// Driven read packets will update the returned data into the same packet.
+// class Axi4MainAgent extends Agent {
+//   /// AXI4 System Interface.
+//   final Axi4SystemInterface sIntf;
+
+//   /// Channels that the agent can send requests on.
+//   final List<Axi4Channel> channels;
+
+//   /// The sequencers where read requests should be sent.
+//   final List<Sequencer<Axi4ReadRequestPacket>> rdSequencers = [];
+
+//   /// The sequencers where write requests should be sent.
+//   final List<Sequencer<Axi4WriteRequestPacket>> wrSequencers = [];
+
+//   /// The drivers that send read requests over the interface.
+//   final List<Axi4ReadMainDriver> rdDrivers = [];
+
+//   /// The drivers that send write requests over the interface.
+//   final List<Axi4WriteMainDriver> wrDrivers = [];
+
+//   /// Monitoring of read requests over the interface.
+//   final List<Axi4ReadMonitor> rdMonitors = [];
+
+//   /// Monitoring of write requests over the interface.
+//   final List<Axi4WriteMonitor> wrMonitors = [];
+
+//   /// The number of cycles before timing out if no transactions can be sent.
+//   final int timeoutCycles;
+
+//   /// The number of cycles before an objection will be dropped when there are
+//   /// no pending packets to send.
+//   final int dropDelayCycles;
+
+//   // capture mapping of channel ID to TB object index
+//   final Map<int, int> _readAddrToChannel = {};
+//   final Map<int, int> _writeAddrToChannel = {};
+
+//   /// Get the ith channel's read sequencer.
+//   Sequencer<Axi4ReadRequestPacket>? getRdSequencer(int channelId) =>
+//       _readAddrToChannel.containsKey(channelId)
+//           ? rdSequencers[_readAddrToChannel[channelId]!]
+//           : null;
+
+//   /// Get the ith channel's write sequencer.
+//   Sequencer<Axi4WriteRequestPacket>? getWrSequencer(int channelId) =>
+//       _writeAddrToChannel.containsKey(channelId)
+//           ? wrSequencers[_writeAddrToChannel[channelId]!]
+//           : null;
+
+//   /// Get the ith channel's read driver.
+//   Axi4ReadMainDriver? getRdDriver(int channelId) =>
+//       _readAddrToChannel.containsKey(channelId)
+//           ? rdDrivers[_readAddrToChannel[channelId]!]
+//           : null;
+
+//   /// Get the ith channel's write driver.
+//   Axi4WriteMainDriver? getWrDriver(int channelId) =>
+//       _writeAddrToChannel.containsKey(channelId)
+//           ? wrDrivers[_writeAddrToChannel[channelId]!]
+//           : null;
+
+//   /// Get the ith channel's read monitor.
+//   Axi4ReadMonitor? getRdMonitor(int channelId) =>
+//       _readAddrToChannel.containsKey(channelId)
+//           ? rdMonitors[_readAddrToChannel[channelId]!]
+//           : null;
+
+//   /// Get the ith channel's write monitor.
+//   Axi4WriteMonitor? getWrMonitor(int channelId) =>
+//       _writeAddrToChannel.containsKey(channelId)
+//           ? wrMonitors[_writeAddrToChannel[channelId]!]
+//           : null;
+
+//   /// Constructs a new [Axi4MainAgent].
+//   Axi4MainAgent({
+//     required this.sIntf,
+//     required this.channels,
+//     required Component parent,
+//     String name = 'axiMainAgent',
+//     this.timeoutCycles = 500,
+//     this.dropDelayCycles = 30,
+//   }) : super(name, parent) {
+//     for (var i = 0; i < channels.length; i++) {
+//       if (channels[i].hasRead) {
+//         rdSequencers.add(Sequencer<Axi4ReadRequestPacket>(
+//             'axiRdSequencer${channels[i].channelId}', this));
+//         _readAddrToChannel[i] = rdSequencers.length - 1;
+//         rdDrivers.add(Axi4ReadMainDriver(
+//           parent: this,
+//           sIntf: sIntf,
+//           rIntf: channels[i].rIntf!,
+//           sequencer: rdSequencers[_readAddrToChannel[i]!],
+//           timeoutCycles: timeoutCycles,
+//           dropDelayCycles: dropDelayCycles,
+//           name: 'axiRdDriver${channels[i].channelId}',
+//         ));
+//         rdMonitors.add(Axi4ReadMonitor(
+//             sIntf: sIntf,
+//             rIntf: channels[i].rIntf!,
+//             parent: parent,
+//             name: 'axiRdMonitor${channels[i].channelId}'));
+//       }
+//       if (channels[i].hasWrite) {
+//         wrSequencers.add(Sequencer<Axi4WriteRequestPacket>(
+//             'axiWrSequencer${channels[i].channelId}', this));
+//         _writeAddrToChannel[i] = wrSequencers.length - 1;
+//         wrDrivers.add(Axi4WriteMainDriver(
+//           parent: this,
+//           sIntf: sIntf,
+//           wIntf: channels[i].wIntf!,
+//           sequencer: wrSequencers[_writeAddrToChannel[i]!],
+//           timeoutCycles: timeoutCycles,
+//           dropDelayCycles: dropDelayCycles,
+//           name: 'axiWrDriver${channels[i].channelId}',
+//         ));
+//         wrMonitors.add(Axi4WriteMonitor(
+//             sIntf: sIntf,
+//             wIntf: channels[i].wIntf!,
+//             parent: parent,
+//             name: 'axiWrMonitor${channels[i].channelId}'));
+//       }
+//     }
+//   }
+// }
