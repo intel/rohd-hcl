de9f2d3c227e5b7ebb80782ac2ac5c4b401b6bad
Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>
2025-03-07 01:33:37 +0800

    1. Fix indexing when read from end 2. Add error signal when pattern is not found
    Signed-off-by: Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>

--- lib/src/find_pattern.dart
+++ lib/src/find_pattern.dart
@@ -1,5 +1,6 @@
 import 'package:rohd/rohd.dart';
 import 'package:rohd_hcl/rohd_hcl.dart';
+import 'package:rohd_vf/rohd_vf.dart';
 
 /// FindPattern functionality
 ///
@@ -9,7 +10,14 @@ class FindPattern extends Module {
   /// [index] is a getter for output of FindPattern
   Logic get index => output('index');
 
-  /// Find a fixed-width pattern
+  /// [error] is a getter for error in FindPattern
+  /// When your pattern is not found it will result in error `1`
+  Logic? get error => tryOutput('error');
+
+  /// If `true`, then the [error] output will be generated.
+  final bool generateError;
+
+  /// Find a position for a fixed-width pattern
   ///
   /// Takes in search pattern [pattern] and a boolean [start] to determine the
   ///  search direction.
@@ -23,7 +31,8 @@ class FindPattern extends Module {
   /// [n] starts from `1` as the first occurrence.
   ///
   /// Outputs pin `index` contains the position. Position starts from `0` based.
-  FindPattern(Logic bus, Logic pattern, {bool start = true, int n = 1})
+  FindPattern(Logic bus, Logic pattern,
+      {bool start = true, int n = 1, this.generateError = false})
       : super(definitionName: 'FindPattern_W${bus.width}_P${pattern.width}') {
     bus = addInput('bus', bus, width: bus.width);
     pattern = addInput('pattern', pattern, width: pattern.width);
@@ -32,70 +41,73 @@ class FindPattern extends Module {
     print('bus: ${bus.value.toInt()}');
     print('pattern: ${pattern.value.toInt()}');
 
-    // Initialize countPatternOccurence to 0
-    int count = 0;
+    // Initialize counter pattern occurrence to 0
+    var count = 0;
 
     print('=======================');
 
     if (start) {
       print('check from start');
       for (var i = 0; i <= bus.width - pattern.width; i = i + 1) {
-        // determines if it is what we are looking for?
-        final busVal = bus.getRange(i, i + pattern.width);
-        final valCheck = bus.getRange(i, i + pattern.width).eq(pattern);
+        // Read from start of bus
+        final minBit = i;
+        final maxBit = i + pattern.width;
+        print('minBit: $minBit, maxBit: $maxBit');
+        final busVal = bus.getRange(minBit, maxBit);
+        // Check if pattern matches
+        final valCheck = busVal.eq(pattern);
         print('i: $i, busVal: ${busVal.value}, valCheck: ${valCheck.value}');
 
         // Check if pattern matches, count if found
-        if (valCheck.value.toInt() == 1) {
-          count = count + 1;
-          print('count: $count');
-          // If pattern is found and n is equal to 1, set index to
-          //current position
-          if (n == 1) {
-            index <= Const(i, width: bus.width);
-            break;
-          }
-          // If pattern is found and count == n, set index to
-          //current position
-          else if (n == count) {
-            print('n == count');
-            index <= Const(i, width: bus.width);
-            break;
-          }
-        }
+        count += (valCheck.value.toInt() == 1) ? 1 : 0;
         print('count: $count');
+
+        // If count matches n, break and return index
+        if (n == count) {
+          print('n == count');
+          index <= Const(i, width: bus.width);
+          print('index: ${index.value}');
+          break;
+        }
         print('=======================');
       }
-      print('index: ${index.value}');
     } else {
       print('check from end');
-      for (var i = bus.width; i >= bus.width - pattern.width; i = i - 1) {
-        // determines if it is what we are looking for?
-        final busVal = bus.getRange(i - pattern.width, i);
-        final valCheck = bus.getRange(i - pattern.width, i).eq(pattern);
+      for (var i = 0; i <= bus.width - pattern.width; i = i + 1) {
+        // Read from end of bus
+        final minBit = bus.width - i - pattern.width;
+        final maxBit = bus.width - i;
+        print('minBit: $minBit, maxBit: $maxBit');
+        final busVal = bus.getRange(minBit, maxBit);
+        print('busVal: ${busVal.value}');
+        // Check if pattern matches
+        final valCheck = busVal.eq(pattern);
         print('i: $i, busVal: ${busVal.value}, valCheck: ${valCheck.value}');
         print('count: $count');
 
         // Check if pattern matches, count if found
-        if (valCheck.value.toInt() == 1) {
-          count = count + 1;
-          print('count: $count');
-          // If pattern is found and n is equal to 1, set index to
-          //current position
-          if (n == 1) {
-            index <= Const(i - 1, width: bus.width);
-            break;
-          }
-          // If pattern is found and count == n, set index to
-          //current position
-          else if (n == count) {
-            print('n == count');
-            index <= Const(i - 1, width: bus.width);
-            break;
-          }
+        count += (valCheck.value.toInt() == 1) ? 1 : 0;
+        print('count: $count');
+
+        // If count matches n, break and return index
+        if (n == count) {
+          print('n == count');
+          index <= Const(i, width: bus.width);
+          print('index: ${index.value}');
+          break;
         }
         print('=======================');
       }
+    }
+
+    if (generateError) {
+      // If pattern is not found, return error
+      var isError = (count < n || count == 0) ? 1 : 0;
+      addOutput('error');
+      error! <= Const(isError, width: 1);
+      print('error: ${error!.value}');
+      // If error is generated, return index as 255
+      index <= Const(255, width: bus.width);
       print('index: ${index.value}');
     }
   }
