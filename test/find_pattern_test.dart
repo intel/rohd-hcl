d9814621285d0f0faf5418d6796fbf428f21d90b
Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>
2025-03-08 16:01:08 +0800

    Update test script to use fromStart
    Signed-off-by: Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>

--- test/find_pattern_test.dart
+++ test/find_pattern_test.dart
@@ -44,26 +44,26 @@ void main() {
     test('at 0th position', () async {
       final bus = Const(bin('11101010'), width: 8);
       final pattern = Const(bin('11101'), width: 5);
-      final findPattern = FindPattern(bus, pattern, start: false);
+      final findPattern = FindPattern(bus, pattern, fromStart: false);
       expect(findPattern.index.value.toInt(), equals(0));
     });
     test('at random position', () async {
       final bus = Const(bin('11011111'), width: 8);
       final pattern = Const(bin('101'), width: 3);
-      final findPattern = FindPattern(bus, pattern, start: false);
+      final findPattern = FindPattern(bus, pattern, fromStart: false);
       expect(findPattern.index.value.toInt(), equals(1));
     });
     test('at last position', () async {
       final bus = Const(bin('10000001'), width: 8);
       final pattern = Const(bin('01'), width: 2);
-      final findPattern = FindPattern(bus, pattern, start: false);
+      final findPattern = FindPattern(bus, pattern, fromStart: false);
       expect(findPattern.index.value.toInt(), equals(6));
     });
     test('Pattern not present', () async {
       final bus = Const(bin('00000000'), width: 8);
       final pattern = Const(bin('111'), width: 3);
       final findPattern =
-          FindPattern(bus, pattern, start: false, generateError: true);
+          FindPattern(bus, pattern, fromStart: false, generateError: true);
       expect(findPattern.error!.value.toInt(), equals(1));
     });
   });
@@ -97,22 +97,22 @@ void main() {
       final bus = Const(bin('01010110'), width: 8);
       final pattern = Const(bin('01'), width: 2);
       final n = Const(0, width: log2Ceil(8) + 1);
-      final findPattern = FindPattern(bus, pattern, start: false, n: n);
+      final findPattern = FindPattern(bus, pattern, fromStart: false, n: n);
       expect(findPattern.index.value.toInt(), equals(0));
     });
     test('where n is 2 (find the 3rd occurrence, n is zero-index)', () async {
       final bus = Const(bin('10101001'), width: 8);
       final pattern = Const(bin('01'), width: 2);
       final n = Const(2, width: log2Ceil(8) + 1);
-      final findPattern = FindPattern(bus, pattern, start: false, n: n);
+      final findPattern = FindPattern(bus, pattern, fromStart: false, n: n);
       expect(findPattern.index.value.toInt(), equals(6));
     });
     test('where n is outside bound', () async {
       final bus = Const(bin('10101001'), width: 8);
       final pattern = Const(bin('01'), width: 2);
       final n = Const(5, width: log2Ceil(8) + 1);
-      final findPattern =
-          FindPattern(bus, pattern, start: false, n: n, generateError: true);
+      final findPattern = FindPattern(bus, pattern,
+          fromStart: false, n: n, generateError: true);
       expect(findPattern.error!.value.toInt(), equals(1));
     });
   });
