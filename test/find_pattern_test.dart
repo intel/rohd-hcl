13035d6f8388b0af00269fc8a1d5205371259188
Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>
2025-03-11 09:44:52 +0800

    Add dynamic input test
    Signed-off-by: Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>

--- test/find_pattern_test.dart
+++ test/find_pattern_test.dart
@@ -6,6 +6,7 @@
 //
 // 2025 March 6
 // Author: Louiz Ang Zhi Lin <louiz.zhi.lin.ang@intel.com>
+// Co-author: Ramli, Nurul Izziany <nurul.izziany.ramli@intel.com>
 //
 
 import 'package:rohd/rohd.dart';
@@ -113,6 +114,98 @@ void main() {
       final n = Const(5, width: log2Ceil(8) + 1);
       final findPattern = FindPattern(bus, pattern,
           fromStart: false, n: n, generateError: true);
+      expect(findPattern.error!.value.toInt(), equals(1));
+    });
+  });
+
+  group('Dynamic input', () {
+    test('where n is null, from start', () async {
+      final bus = Logic(width: 8);
+      final pattern = Logic(width: 2);
+      final findPattern = FindPattern(bus, pattern, generateError: true);
+
+      bus.put(bin('10000001'));
+      pattern.put(bin('01'));
+      expect(findPattern.index.value.toInt(), equals(0));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('11011111'));
+      pattern.put(bin('10'));
+      expect(findPattern.index.value.toInt(), equals(5));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('11110111'));
+      pattern.put(bin('00'));
+      expect(findPattern.error!.value.toInt(), equals(1));
+    });
+
+    test('where n is defined, from start', () async {
+      final bus = Logic(width: 8);
+      final pattern = Logic(width: 2);
+      final n = Logic(width: log2Ceil(8) + 1);
+      final findPattern = FindPattern(bus, pattern, n: n, generateError: true);
+
+      bus.put(bin('10110101'));
+      pattern.put(bin('01'));
+      n.put(1);
+      expect(findPattern.index.value.toInt(), equals(2));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('10010101'));
+      pattern.put(bin('10'));
+      n.put(2);
+      expect(findPattern.index.value.toInt(), equals(6));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('11100100'));
+      pattern.put(bin('00'));
+      n.put(3);
+      expect(findPattern.error!.value.toInt(), equals(1));
+    });
+
+    test('where n is null, from end', () async {
+      final bus = Logic(width: 8);
+      final pattern = Logic(width: 2);
+      final findPattern =
+          FindPattern(bus, pattern, fromStart: false, generateError: true);
+
+      bus.put(bin('10000001'));
+      pattern.put(bin('01'));
+      expect(findPattern.index.value.toInt(), equals(6));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('11011111'));
+      pattern.put(bin('10'));
+      expect(findPattern.index.value.toInt(), equals(1));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('11110111'));
+      pattern.put(bin('00'));
+      expect(findPattern.error!.value.toInt(), equals(1));
+    });
+
+    test('where n is defined, from end', () async {
+      final bus = Logic(width: 8);
+      final pattern = Logic(width: 2);
+      final n = Logic(width: log2Ceil(8) + 1);
+      final findPattern = FindPattern(bus, pattern,
+          fromStart: false, n: n, generateError: true);
+
+      bus.put(bin('10110101'));
+      pattern.put(bin('01'));
+      n.put(1);
+      expect(findPattern.index.value.toInt(), equals(4));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('10010101'));
+      pattern.put(bin('10'));
+      n.put(2);
+      expect(findPattern.index.value.toInt(), equals(5));
+      expect(findPattern.error!.value.toInt(), equals(0));
+
+      bus.put(bin('11100100'));
+      pattern.put(bin('11'));
+      n.put(3);
       expect(findPattern.error!.value.toInt(), equals(1));
     });
   });
