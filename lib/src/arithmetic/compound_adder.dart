9d7f767a2635ffe087a537341b0957be03de988b
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2025-04-23 08:55:21 -0700

    cleanup verilog output for compound_adder

--- lib/src/arithmetic/compound_adder.dart
+++ lib/src/arithmetic/compound_adder.dart
@@ -126,8 +126,8 @@ class CarrySelectCompoundAdder extends CompoundAdder {
         ? addInput('subtractIn', subtractIn, width: subtractIn.width)
         : null;
     // output bits lists
-    final sumList0 = <Logic>[];
-    final sumList1 = <Logic>[];
+    final sumList0F = <Logic>[];
+    final sumList1F = <Logic>[];
     // carryout of previous adder block
     // for sum and sum+1
     Logic carry0 = Const(0);
@@ -139,58 +139,63 @@ class CarrySelectCompoundAdder extends CompoundAdder {
     for (var i = 0; i < adderSplit.length; ++i) {
       // input width of current adder block
       final blockWidth = adderSplit[i];
+      final sum0Ary = Logic(width: blockWidth);
+      final sum1Ary = Logic(width: blockWidth);
       if (blockWidth <= 0) {
         throw RohdHclException('non-positive adder block size.');
       }
       if (blockWidth + blockStartIdx > a.width) {
         throw RohdHclException('oversized adders sequence.');
       }
-      final blockA = Logic(name: 'block_${i}_a', width: blockWidth);
-      final blockB = Logic(name: 'block_${i}_b', width: blockWidth);
-      blockA <= a.getRange(blockStartIdx, blockStartIdx + blockWidth);
-      blockB <= b.getRange(blockStartIdx, blockStartIdx + blockWidth);
       // Build sub adders for 0 and 1 carryin values
-      final fullAdder0 = adderGen(blockA, blockB,
-          subtractIn: subtractIn, carryIn: Const(0), name: 'block0_$i');
-      final fullAdder1 = adderGen(blockA, blockB,
-          subtractIn: subtractIn, carryIn: Const(1), name: 'block1_$i');
-      for (var bitIdx = 0; bitIdx < blockWidth; ++bitIdx) {
-        if (i == 0) {
-          // connect directly to respective sum output bit
-          sumList0.add(fullAdder0.sum[bitIdx]);
-          sumList1.add(fullAdder1.sum[bitIdx]);
-        } else {
-          final bitOut0 = Logic(name: 'bit0_${blockStartIdx + bitIdx}');
-          final bitOut1 = Logic(name: 'bit1_${blockStartIdx + bitIdx}');
-          // select adder output from adder matching carryin value
-          bitOut0 <=
-              mux(carry0, fullAdder1.sum[bitIdx], fullAdder0.sum[bitIdx]);
-          bitOut1 <=
-              mux(carry1, fullAdder1.sum[bitIdx], fullAdder0.sum[bitIdx]);
-          sumList0.add(bitOut0);
-          sumList1.add(bitOut1);
-        }
-      }
+      final fullAdder0 = adderGen(
+          a.getRange(blockStartIdx, blockStartIdx + blockWidth),
+          b.getRange(blockStartIdx, blockStartIdx + blockWidth),
+          subtractIn: subtractIn,
+          carryIn: Const(0),
+          name: 'block0_$i');
+      final fullAdder1 = adderGen(
+          a.getRange(blockStartIdx, blockStartIdx + blockWidth),
+          b.getRange(blockStartIdx, blockStartIdx + blockWidth),
+          subtractIn: subtractIn,
+          carryIn: Const(1),
+          name: 'block1_$i');
+      sum0Ary <=
+          [
+            for (var bitIdx = 0; bitIdx < blockWidth; ++bitIdx)
+              (i == 0)
+                  ? fullAdder0.sum[bitIdx]
+                  : mux(carry0, fullAdder1.sum[bitIdx], fullAdder0.sum[bitIdx])
+          ].swizzle().named('block_${i}_sum0Ary');
+      sum1Ary <=
+          [
+            for (var bitIdx = 0; bitIdx < blockWidth; ++bitIdx)
+              (i == 0)
+                  ? fullAdder1.sum[bitIdx]
+                  : mux(carry1, fullAdder1.sum[bitIdx], fullAdder0.sum[bitIdx])
+          ].swizzle().named('block_${i}_sum1Ary');
+      sumList0F.add(sum0Ary);
+      sumList1F.add(sum1Ary);
       if (i == 0) {
         // select carryout as a last bit of the adder
-        carry0 = fullAdder0.sum[blockWidth];
-        carry1 = fullAdder1.sum[blockWidth];
+        carry0 = fullAdder0.sum[blockWidth].named('block_${i}_adder0Msb');
+        carry1 = fullAdder1.sum[blockWidth].named('block_${i}_adder1Msb');
       } else {
         // select carryout depending on carryin (carryout of the previous block)
         carry0 =
-            mux(carry0, fullAdder1.sum[blockWidth], fullAdder0.sum[blockWidth]);
+            mux(carry0, fullAdder1.sum[blockWidth], fullAdder0.sum[blockWidth])
+                .named('block_${i}_carry0');
         carry1 =
-            mux(carry1, fullAdder1.sum[blockWidth], fullAdder0.sum[blockWidth]);
+            mux(carry1, fullAdder1.sum[blockWidth], fullAdder0.sum[blockWidth])
+                .named('block_${i}_carry1');
       }
+
       blockStartIdx += blockWidth;
     }
 
-    // Append carryout bit
-    sumList0.add(carry0);
-    sumList1.add(carry1);
-
-    sum <= sumList0.rswizzle();
-    sumP1 <= sumList1.rswizzle();
+    sum <= [sumList0F.swizzle(), carry0].swizzle().named('sum_inner').reversed;
+    sumP1 <=
+        [sumList1F.swizzle(), carry1].swizzle().named('sumP1_inner').reversed;
   }
 }
 
