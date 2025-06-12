24c47e6a7fa6d22b0ffc1488d72a0fc1625edd8d
Max Korbel <max.korbel@intel.com>
2025-06-12 08:54:44 -0700

    Add error generation to tree one hot to binary (#211)

--- lib/src/utils.dart
+++ lib/src/utils.dart
@@ -10,6 +10,9 @@ import 'package:rohd/rohd.dart';
 /// Computes the bit width needed to store [w] addresses.
 int log2Ceil(int w) => (log(w) / log(2)).ceil();
 
+/// Returns whether [n] is a power of two.
+bool isPowerOfTwo(int n) => n != 0 && (n & (n - 1) == 0);
+
 /// This extension will eventually move to ROHD once it is proven useful
 extension LogicValueBitString on LogicValue {
   /// Simplest version of bit string representation as shorthand
