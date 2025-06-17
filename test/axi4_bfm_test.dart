af5511d7597abe42675222ad5266a2a7d0394462
Kimmel, Joshua1 <joshua1.kimmel@intel.com>
2025-06-17 13:09:23 -0700

    last feedback fixes

--- test/axi4_bfm_test.dart
+++ test/axi4_bfm_test.dart
@@ -1143,7 +1143,7 @@ void main() {
 
   setUp(() async {
     // Set the logger level
-    // Logger.root.level = Level.SEVERE;
+    Logger.root.level = Level.OFF;
   });
 
   Future<void> runTest(Axi4BfmTest axi4BfmTest,
