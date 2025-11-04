8fe97c39787ab6778ac5fb7bb9811a66058d5e59
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2025-11-04 15:00:57 -0800

    directory reorg for cache components

--- lib/src/memory/memories.dart
+++ lib/src/memory/memories.dart
@@ -1,15 +1,13 @@
 // Copyright (C) 2024-2025 Intel Corporation
 // SPDX-License-Identifier: BSD-3-Clause
 
-export 'cache.dart';
-export 'cached_request_response.dart';
+export 'buffered_request_response_channel.dart';
+export 'cache/cache.dart';
 export 'cam.dart';
 export 'csr/csrs.dart';
 export 'fifo.dart';
 export 'memory.dart';
-export 'pseudo_lru_replacement.dart';
 export 'ready_valid_fifo.dart';
 export 'register_file.dart';
-export 'replacement_policy.dart';
 export 'resettable_entries.dart';
 export 'shift_register.dart';
