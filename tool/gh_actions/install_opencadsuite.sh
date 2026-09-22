306a6dfd67faec5c4417e68f3d296ee02e20cfd2
Desmond A. Kirkpatrick <desmond.a.kirkpatrick@intel.com>
2026-09-21 23:06:57 -0700

    restore opencadsuite install

---
+++ tool/gh_actions/install_opencadsuite.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Copyright (C) 2023-2024 Intel Corporation
+# SPDX-License-Identifier: BSD-3-Clause
+#
+# install_opencadsuite.sh
+# GitHub Actions step: Install project dependencies.
+#
+# 2023 May 09
+# Author: Desmond Kirkpatrick <desmond.a.kirkpatrick@intel.com>
+
+set -euo pipefail
+
+sudo apt-get update
+sudo apt-get install -y \
+	git \
+	npm \
+	python3-pip
+
+cd /
+sudo  wget -O oss-cad-suite-build.tgz https://github.com/YosysHQ/oss-cad-suite-build/releases/download/2025-07-10/oss-cad-suite-linux-x64-20250710.tgz
+
+sudo tar -xzf oss-cad-suite-build.tgz
+
+# Trim if needed
